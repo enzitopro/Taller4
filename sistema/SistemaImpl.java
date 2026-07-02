f0770365f4c8d5c35a1aa0d9cc9dbfdc3a840706
enzitopro <ajipicantemrdayt@gmail.com>
2026-07-02 03:56:11 -0400

    add singleton a sistemaimpl

--- sistema/SistemaImpl.java
+++ sistema/SistemaImpl.java
@@ -1,5 +1,25 @@
 package sistema;
 
-public class SistemaImpl {
+import java.util.ArrayList;
+import java.util.List;
 
+import dominio.Carta;
+
+public class SistemaImpl {
+	private static SistemaImpl instancia;
+    private List<Carta> coleccion;
+    private final String RUTA_ARCHIVO = "Sobres.txt";
+
+    // 1. PATRÓN SINGLETON: Constructor privado
+    private SistemaImpl() {
+        coleccion = new ArrayList<>();
+        cargarDatos();
+    }
+    
+    public static SistemaImpl getInstancia() {
+        if (instancia == null) {
+            instancia = new SistemaImpl();
+        }
+        return instancia;
+    }
 }
