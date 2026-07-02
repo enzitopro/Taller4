627fb5658670ca4c1abd7e29f5ffc5019a3461a9
enzitopro <ajipicantemrdayt@gmail.com>
2026-07-02 03:48:13 -0400

    add integrantes y link repo

--- sistema/App.java
+++ sistema/App.java
@@ -1,5 +1,9 @@
 package sistema;
 
+// Integrante 1: Elliot Bravo de Rodt - enzitopro
+// Integrante 2: Enzo Salvatore Cornieles Medina - justamago
+// Link repositorio: https://github.com/enzitopro/Taller3
+
 public class App {
 
 }
